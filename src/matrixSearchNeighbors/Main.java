e47f3ce3068c5829778b6e7cff0e44c46ebcfa82
gabrielbassanicode <gabrielbassani.code@gmail.com>
2026-08-17 13:48:48 -0300

    Fixes

--- src/matrixSearchNeighbors/Main.java
+++ src/matrixSearchNeighbors/Main.java
@@ -1,9 +1,9 @@
-package matrixSearchNeighbors.application;
+package matrixSearchNeighbors;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class Main {
 
     public static void main(String[] args) {
 
